d05634b3522b8a6a9c2a966f7908f4463fbfa1b5
randomblitz9 <randomblitz9@gmail.com>
2018-12-19 16:14:24 +0800

    Initial Commit

---
+++ first.js
@@ -0,0 +1,203 @@
+
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host     : '127.0.0.1',
+  user     : 'root',
+  password : '',
+  database : 'inventory'
+});
+const bodyParser = require('body-parser');
+
+class Item {
+  constructor() {
+    this.id = null;
+  }
+  
+    set id(id) {
+    this._id = id;
+  }
+  get amount() {
+    return this._id;
+  }
+  set amount(amount) {
+    this._amount = amount;
+  }
+  get amount() {
+    return this._amount;
+  }
+  
+    set name(name) {
+    this._name = name;
+  }
+  get name() {
+    return this._name;
+  }
+  
+  
+    set qty(qty) {
+    this._qty =qty;
+		}	
+  get qty() {
+    return this._qty
+  }
+  
+}
+
+
+
+
+
+
+const express = require('express')
+const app = express()
+app.set('view engine', 'ejs');
+const port = 3000
+//connection.connect()
+
+app.use(bodyParser.urlencoded({ extended: false }));
+	var array = [];
+//connection.end()
+
+connection.query('SELECT * FROM items', function (err, rows, fields) {
+		
+  if (err) throw err
+
+
+
+  
+		  rows.forEach(function(element) {
+		  console.log(element.name);
+	 var t = new Item();
+		 t.qty=element.qty;
+		 t.name = element.name;
+		 t.id = element.id;
+		 		 t.amount= element.amount;
+		 array.push(t);
+		 
+		});
+	
+})
+app.get('/', function(req, res) {
+
+	connection.query('SELECT * FROM items', function (err, rows, fields) {
+		array = [];
+  if (err) throw err
+
+
+
+  
+		  rows.forEach(function(element) {
+		  console.log(element.name);
+	 var t = new Item();
+		 t.qty=element.qty;
+		 t.name = element.name;
+		 t.id = element.id;
+		 		 t.amount= element.amount;
+		 array.push(t);
+		 
+		});
+	
+})
+
+res.send(array);
+array = [];
+});
+
+
+	 var tt = new Item();
+
+app.get('/:tagId', function(req, res) {
+	tt = new Item();
+		connection.query('SELECT * FROM items WHERE items.id = ?',[req.params.tagId], function (err, rows, fields) {
+  if (err) throw err
+
+
+  
+		  rows.forEach(function(element) {
+		
+	
+		 tt.qty=element.qty;
+		tt.name = element.name;
+		tt.id = element.id;
+		 		 tt.amount= element.amount;
+res.send(tt);
+		});
+	
+})
+
+
+
+});
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+app.delete('/d/:tagId', function(req, res) {
+	let sql = `DELETE FROM items WHERE items.id = ?`;
+   let post = [req.params.tagId];
+    connection.query(sql, post, (err, result) => {
+        if(err){
+            console.log(err)
+        }
+ 
+     
+
+    });
+	
+  res.send("tagId is set to " + req.params.tagId);
+});
+
+app.post('/', function(req, res) {
+			var t = new Item();
+		 t.qty=req.body._qty;
+		 t.name = req.body._name;
+
+		 t.amount = req.body._amount;
+console.log("inserting");
+
+
+ let sql = "INSERT INTO items (id, name, qty, amount) VALUES (?,?, ?, ?)";
+   let post = [null,t.name,t.qty, t.amount];
+    connection.query(sql, post, (err, result) => {
+        if(err){
+            console.log(err)
+        }
+ 
+     
+
+    });
+
+res.sendStatus(200);
+ })
+ 
+
+app.put('/', function(req, res) {
+			var t = new Item();
+			t.id = req.body._id;
+		 t.qty=req.body._qty;
+		 t.name = req.body._name;
+
+		 t.amount = req.body._amount;
+
+            console.log("editing"+JSON.stringify(t, null, 4));
+
+
+ let sql = 'UPDATE items SET name = "'+t.name+'", qty = "'+t.qty+'", amount = "'+t.amount+'" WHERE items.id = '+ req.body._id;
+   let post = [t.name,t.qty, t.amount,t.id];
+    connection.query(sql, post, (err, result) => {
+            console.log("editing"+JSON.stringify(result, null, 4));
+
+        if(err){
+            console.log(err)
+        }
+ 
+     
+
+    });
+
+	
+	
+res.sendStatus(200);
+ })
+ 
---
+++ viewengine.js
@@ -0,0 +1,225 @@
+
+var http = require('http');
+const bodyParser = require('body-parser');
+
+const express = require('express')
+const app = express()
+app.set('view engine', 'ejs');
+const port = 3001
+//connection.connect()
+
+const request = require('request');
+//var justAGuy = new Person();
+//justAGuy.name = 'martin'; // The setter will be used automatically here.
+//justAGuy.sayHello(); // Will output 'Hello, my name is Martin, I have ID: id_1'
+app.disable('view cache');
+
+class Item {
+  constructor() {
+    this.id = null;
+  }
+  
+    set id(id) {
+    this._id = id;
+  }
+  get amount() {
+    return this._id;
+  }
+  set amount(amount) {
+    this._amount = amount;
+  }
+  get amount() {
+    return this._amount;
+  }
+  
+    set name(name) {
+    this._name = name;
+  }
+  get name() {
+    return this._name;
+  }
+  
+  
+    set qty(qty) {
+    this._qty =qty;
+		}	
+  get qty() {
+    return this._qty
+  }
+  //sayHello() {
+  //  console.log('Hello, my name is ' + this.name + ', I have ID: ' + this.id);
+  //}
+}
+
+
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+// server.js
+		
+		
+							
+var json;
+// index page 
+
+app.get('/', function(req, res) {
+	const url = "http://127.0.0.1:3000/";
+					request.get(url, (error, response, body) => {
+			   json = JSON.parse(body);
+			 
+
+			  	res.render('pages/index', {
+						  inventories: json,
+
+						});
+			  
+			});
+					
+				 console.log(json);
+					
+		
+		
+		
+});
+
+app.get('/new', function(req, res) {
+
+					
+				res.render('pages/new_forrm', {
+					
+
+						});
+					
+				json=null;
+		
+});
+
+app.get('/m/:tagId', function(req, res) {
+				const url = "http://127.0.0.1:3000/"+req.params.tagId;
+					request.get(url, (error, response, body) => {
+			  let json = JSON.parse(body);
+			  console.log(body);
+			  inv = json;
+			  	res.render('pages/modify_form', {
+						  json: json,
+
+						});
+			  
+			});
+					
+				
+			});
+	
+
+
+
+function sleep(time, callback) {
+    var stop = new Date().getTime();
+    while(new Date().getTime() < stop + time) {
+        ;
+    }
+    callback();
+}
+
+
+app.post('/m/:tagId', function(req, res) {
+			var t = new Item();
+					 t.id=req.params.tagId;
+
+		 t.qty=req.param('qty');
+		 t.name = req.param('name');
+		 t.amount =req.param('amount');
+			
+							request.put({
+				headers: {'content-type': 'application/json'},
+				url: 'http://127.0.0.1:3000/',
+				form: t
+			}, function(error, response, body){
+			  console.log(body)
+			  
+			  if(response.body=="OK"){
+			const urlz = "http://127.0.0.1:3000/";
+					request.get(urlz, (error, response, body) => {
+			
+
+		
+						  res.redirect('../');
+					});
+					
+			  }
+	
+
+			});
+						
+
+				
+				
+		     console.log("editing"+JSON.stringify(t, null, 4));
+	
+});
+
+app.post('/new', function(req, res) {
+			
+									var t = new Item();
+								t.qty=req.param('qty');
+								t.name = req.param('name');
+								t.amount = req.param('amount');
+					
+								
+
+
+						request.post({
+							headers: {'content-type': 'application/json'},
+							url: 'http://127.0.0.1:3000/',
+							form: t
+						}, function(error, response, body){
+						  console.log(body)
+						});
+													
+											const urlz = "http://127.0.0.1:3000/";
+					request.get(urlz, (error, response, body) => {
+			
+
+		
+						  res.redirect('../');
+					});
+					
+});
+
+
+
+app.get('/del/:tagId', function(req, res) {
+			
+				
+	
+				
+
+
+request.delete({
+    headers: {'content-type': 'application/json'},
+    url: 'http://127.0.0.1:3000/d/'+req.params.tagId
+    
+}, function(error, response, body){
+  console.log(body)
+});
+							
+					const urlz = "http://127.0.0.1:3000/";
+					request.get(urlz, (error, response, body) => {
+			
+
+		
+						  res.redirect('../');
+					});
+					
+			
+				  	
+			  
+});
+
+
+
